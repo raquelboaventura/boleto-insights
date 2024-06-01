d7e8d6a95e6becb8c34f5fc69f9f82f9e57e0c4a
raquelboaventura <raquelb_moraes@live.com>
2024-06-01 20:01:45 -0300

    :boom: feat: botão de gerar insights e response na tela

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ async def home():
     return await render_template('index.html')
 
 
-@app.route("/insights", methods=["POST"])
+@app.route("/insights", methods=["GET"])
 async def insights():
     try:
         insights_result = await criar_tabelas()
@@ -31,22 +31,29 @@ async def insights():
         logger.error(f"Erro geral: {e}")
         return jsonify({"message": "Erro ao processar a solicitação", "status_code": 500}), 500
 
-
-@app.route('/upload', methods=['POST'])
+@app.route("/upload", methods=["POST"])
 async def upload_file():
-    # Verifica se foi enviado um arquivo na requisição
-    if 'file' not in (await request.files):
+    logger.info("entrando na requisição /upload")
+    if 'files[]' not in (await request.files):
+        logger.info("Nenhum arquivo identificado :(")
         return jsonify({'error': 'Nenhum arquivo enviado'}), 400
 
-    file = (await request.files)['file']
+    files = (await request.files).getlist('files[]')
+    logger.info(f"FILES: {files}")
 
-    # Verifica se o arquivo é um PDF
-    if file.filename.endswith('.pdf'):
-        # Salva o arquivo no diretório desejado
-        await file.save(os.path.join('..\src\input', file.filename))
-        return jsonify({'message': 'Arquivo enviado com sucesso'}), 200
-    else:
-        return jsonify({'error': 'Arquivo enviado não é um PDF'}), 400
+    for file in files:   # Verifica se o arquivo é um PDF
+        logger.info(f"entrando no loop: {file}")
+        if file.filename.endswith('.pdf'):
+            # Salva o arquivo no diretório desejado
+            await file.save(os.path.join('..\src\input', file.filename))
+            logger.info(f"arquivo salvo com sucesso! -> {file}")
+        else:
+            logger.error(f"deu ruim: {file}")
+            return jsonify({'error': 'Arquivo enviado não é um PDF'}), 400
+
+    logger.info("arquivos processados com sucesso! /upload finalizado.")
+    return jsonify({'message': 'Arquivos enviados com sucesso'}), 200
+
 
 
 if __name__ == "__main__":
--- src/static/script.js
+++ src/static/script.js
@@ -4,16 +4,96 @@ let sendBtn = document.querySelector("#sendBtn")
 let selectBtn = document.querySelector("#selectBtn")
 let uploadBtn = document.querySelector("#fileInput")
 
-//teste clicks 
-sendBtn.addEventListener('click', async () => {
-    console.log("Send button clicked!");})
+// Arquivo
+let selectedFiles = [];
 
+
+function setupInsightsBtn() {
+    let insightsBtn = document.querySelector("#imgBtnInsights");
+    if (insightsBtn) {
+        insightsBtn.addEventListener('click', async () => {
+            try {
+                console.log("btninsights clicadooooooooooo");
+                const response = await fetch('/insights', {
+                    method: 'GET'
+                });
+
+                const result = await response.json();
+                if (response.ok) {
+                    console.log("foiiiiiiiiiiiiiiiiiiiiiii");
+                    let responseContent = document.querySelector("#service #content #right #response #content");
+
+                    if (responseContent) {
+                        // Remove a imagem de fundo
+                        responseContent.style.backgroundImage = 'none';
+
+                        // Adiciona o texto ao conteúdo do elemento
+                        responseContent.textContent = result.message;
+                    } else {
+                        console.error('Elemento não encontrado!');
+                    }
+
+                    console.log(result.message);
+                } else {
+                    console.log("nao foiii =(");
+                    console.log(result.error);
+                }
+            } catch (error) {
+                console.error('Erro ao obter insights:', error);
+            }
+        });
+    }
+}
+
+// Evento de clique para enviar arquivos
+sendBtn.addEventListener('click', async () => {
+    selectedFiles = Array.from(uploadBtn.files);
+
+    // Verifica se nenhum arquivo foi selecionado
+    if (selectedFiles.length === 0) {
+        console.log("Nenhum arquivo selecionado!");
+        return;
+    }
+
+    const formData = new FormData();
+    for (const file of selectedFiles) {
+        formData.append('files[]', file);
+    }
+
+    try {
+        const response = await fetch('/upload', {
+            method: 'POST',
+            body: formData
+        });
+
+        const result = await response.json();
+        if (response.ok) {
+            console.log(result.message);
+            // Muda a imagem e o ID do botão após o envio bem-sucedido
+            sendBtn.src = "./static/img/create-insights.svg";
+            sendBtn.id = 'imgBtnInsights';
+
+            // Configura o evento de clique para o novo botão
+            setupInsightsBtn();
+        } else {
+            console.log(result.error);
+        }
+    } catch (error) {
+        console.error('Erro ao enviar os arquivos:', error);
+    }
+});
+
+// Evento de clique para selecionar arquivos
 selectBtn.addEventListener('click', (e) => {
     console.log("selectBtn button clicked!");
     e.preventDefault();
     uploadBtn.click(); // Simula um clique no uploadBtn
 });
 
+// Evento para capturar a mudança no arquivo selecionado
 uploadBtn.addEventListener('change', async () => {
-    console.log("uploadBtn opaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+    console.log("uploadBtn change event");
 });
+
+// Configura o evento de clique para o botão de insights inicial
+setupInsightsBtn();
